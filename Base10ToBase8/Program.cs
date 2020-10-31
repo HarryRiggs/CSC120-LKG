66e340f95bbd0d5fd4cb76c417bdeffffc13326a
Harry <hriggs111@gmail.com>
2020-10-30 17:27:58 -0700

    Wrote base 10 converter to base 8

---
+++ Base10ToBase8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base10ToBase8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Decimal: ");
+            int decimalNumber = int.Parse(Console.ReadLine());
+            int remainder;
+            string result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 8;
+                decimalNumber /= 8;
+                result = remainder.ToString() + result;
+            }
+            Console.WriteLine("Octal base 8: {0}", result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
